43da1a05ec0adcbbee2b2bfaf833008c1ea73876
makdadejene <108958177+makdadejene@users.noreply.github.com>
2023-03-13 18:21:13 -0400

    Create DedupArray.java

---
+++ Assignment-1/DedupArray.java
@@ -0,0 +1,25 @@
+public class DedupArray {
+  
+  public void removeDuplicates {
+
+    if(nums.length == 0 || nums.length == 1)
+      return;
+       
+       
+    int j = 1;
+       
+      
+    for (int i = 1; i < nums.length; i++) {
+       if(nums[i] != nums[i-1]) {
+           nums[j] = nums[i];
+           j++;
+        }
+    }
+                
+     
+    while(j<nums.length) {
+       nums[j] = -1;
+       j++;
+    }
+  }
+}
