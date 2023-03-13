b019fe70fd218b6eb61d89d9691ba02a15333912
makdadejene <108958177+makdadejene@users.noreply.github.com>
2023-03-13 18:09:17 -0400

    Create TwoSum.java

---
+++ Assignment-1/TwoSum.java
@@ -0,0 +1,23 @@
+public class TwoSum {
+  
+  public getTwoSum(int[] nums, int k) {
+     HashMap<Integer, Integer> map = new HashMap();
+    
+    
+       int count = 0;
+
+       for(int i = 0; i<nums.length; i++) {
+           if(map.containsKey(k-nums[i])) {
+               count += map.get(k-nums[i]);
+           }
+
+           if(map.containsKey(nums[i])) {
+               map.put(nums[i], map.get(nums[i])+1);
+           } else {
+                map.put(nums[i], i);
+           }  
+       }
+       return count;
+  }
+  
+}
