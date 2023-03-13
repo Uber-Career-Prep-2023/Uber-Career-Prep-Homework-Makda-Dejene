11f05f0aa7d407cdced945dd817e011606843014
makdadejene <108958177+makdadejene@users.noreply.github.com>
2023-03-13 17:59:06 -0400

    Create MissingInteger.java

---
+++ Assignment-1/MissingInteger.java
@@ -0,0 +1,11 @@
+public class MissingInteger{
+
+  public int missingNumber(int[] nums) {
+    int sum = (nums.length + 1) * nums.length / 2;
+    for (int i : nums) {
+      sum -= i;
+    }
+    return sum;
+
+  }
+}
